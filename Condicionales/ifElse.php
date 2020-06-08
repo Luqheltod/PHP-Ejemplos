732661171cf2b84199e33ab5da139a5cb03c89f4
GSancho <romul@DESKTOP-M0A3UP8>
2020-06-08 18:49:20 +0200

    If, Else,

---
+++ Condicionales/ifElse.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> No title yet </title>
+</head>
+<body>
+
+<?php 
+$x =6;
+
+if($x>3){
+    
+    echo "es mayor que 3";
+}
+
+?>
+
+</body>
+
+</html>
