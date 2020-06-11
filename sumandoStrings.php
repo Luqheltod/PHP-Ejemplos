749866a60124ad1082bb03818f9f985a160c94a0
Gsancho <luqh555@gmail.com>
2020-06-11 17:45:28 +0200

    Sencillo

---
+++ sumandoStrings.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+//Sumando Strings: 
+
+
+$string1 = "Sumamos esto ";
+$string2 = "a esto otro"; 
+$resultado = $string1 . $string2; //en la variable resultado se encuentra ahora la cadena "Sumamos esto a esto otro"
+echo $resultado; //mostramos la cadena
+
+
+
+?>
