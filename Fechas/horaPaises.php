b52f5f4bf7d1d26214056b595ed45f88d048b2bc
Gsancho <luqh555@gmail.com>
2020-06-12 18:45:59 +0200

    Sencillos Fechas

---
+++ Fechas/horaPaises.php
@@ -0,0 +1,27 @@
+<?php
+
+ini_set('date.timezone','Europe/Moscow');
+echo "Hora en Moscú: " . date("g:i A") . "<br>";
+
+
+ini_set('date.timezone','America/Cancun');
+echo "Hora en Cancún: " . date("g:i A") . "<br>";
+
+
+
+ini_set('date.timezone','Pacific/Tahiti');
+echo "Hora en Tahití: " . date("g:i A") . "<br>";
+
+
+
+ini_set('date.timezone','Asia/Tokyo');
+echo "Hora en Tokio: " . date("g:i A") . "<br>";
+
+
+
+ini_set('date.timezone','Asia/Hong_Kong');
+echo "Hora en Hong Kong: " . date("g:i A") . "<br>";
+
+
+
+?>
