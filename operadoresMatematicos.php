671a638f1ac8f7a34544a25a58043e6e152dd8e6
Gsancho <luqh555@gmail.com>
2020-06-12 12:33:28 +0200

    Sencillos

---
+++ operadoresMatematicos.php
@@ -0,0 +1,45 @@
+<?php
+
+
+//gsprogramacion.com
+
+//Operadores Matematicos
+
+
+$num1 = 25 ;
+$num2 = 10;
+
+// Suma : Se realiza con el operador  +
+
+echo $num1 + $num2 ;  // devuelve 35
+echo "<br>";
+
+
+
+//Resta :  Se realiza con el operador  -
+
+echo $num1 - $num2; // devuelve 15
+echo "<br>";
+
+
+
+//Multiplicacion:  Se realiza con el operador  *
+
+echo $num1*$num2; // devuelve 250
+echo "<br>";
+
+
+
+//Division :  Se realiza con el operador  /
+
+echo $num1/$num2; // devuelve 2.5
+echo "<br>";
+
+
+
+//Modulo o resto :  Se realiza con el operador  %
+
+echo $num1%$num2; // devuelve 5
+echo "<br>";
+
+?>
