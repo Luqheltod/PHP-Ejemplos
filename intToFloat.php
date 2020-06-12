3c6d0cc2a61956f51190a97cefb346f5e26881d2
Gsancho <luqh555@gmail.com>
2020-06-12 10:37:27 +0200

    Sencillos

---
+++ intToFloat.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+
+$numeraco = 214748364 ;
+
+echo "Este es el numeraco y es " . gettype($numeraco);  //aqui podemos ver que devuelve integer.
+
+echo "<br>";
+
+$numeraco = $numeraco +10000000000000000000; //le sumamos un numera consistentemente grande al numeraco
+
+echo "Este es el numeraco y es " . gettype($numeraco); // ahora podemos observar que devuelve double. La conversion es automatica
+
+
+
+?>
