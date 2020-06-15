1c57a800ef4cba30bed00ead89888ec21e5a5ca8
Gsancho <luqh555@gmail.com>
2020-06-15 18:18:18 +0200

    Sencillos

---
+++ Bucles/forSencillo.php
@@ -0,0 +1,21 @@
+<?php
+
+//gsprogramacion.com
+
+$variableCualquiera  = 10 ;
+
+
+/* En el primer argumento declaramos una variable,
+ * En el segundo ponemos la condicion que hara que el bucle continue (es como una condicion IF , si $i vale menos que 10, el bucle seguira)
+ * . En el tercero se incrementa la variable en uno, para que el bucle no continue infinitamente.
+ *
+ */
+for ($i = 0; $i < $variableCualquiera; $i++) {
+    
+    echo "ejecutaremos esta linea 10 veces <br>";
+    
+    
+    
+}
+
+?>
