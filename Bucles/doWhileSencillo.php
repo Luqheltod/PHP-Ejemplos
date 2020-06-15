733c262686172ae58c0388329b9683bedf345bab
Gsancho <luqh555@gmail.com>
2020-06-15 18:21:56 +0200

    Sencillos

---
+++ Bucles/doWhileSencillo.php
@@ -0,0 +1,22 @@
+<?php
+
+//gsprogramacion.com
+
+$x = 3 ;
+
+
+//Se declara un do, se abre llave, se introduce el codigo que queramos que se ejecute.
+//En caso de que la condicion se cumpla, volvera a ejecutarse de nuevo como si fuese un while
+
+do {
+    
+    
+    echo "esta linea se escribira solo una vez";
+    
+    
+    
+} while ( $x > 10 );   // la condicion no se cumple, sin embargo gracias al do while, al menos se ejecutara una vez
+
+
+
+?>
