a1cd05c39554e71da370cfd64eaa253cf832c79f
Gsancho <luqh555@gmail.com>
2020-06-15 18:08:20 +0200

    Sencillos

---
+++ Bucles/whileSencillo.php
@@ -0,0 +1,14 @@
+
+
+<?php
+// gsprogramacion.com
+
+$variable = 1;
+while ($variable <= 10) {  //Mientras que la condicion se cumple, que la variable valga menos o igual a 10, se continuara ejecutando
+    echo "la variable  vale " . $variable . " No ejecutaremos mas este codigo cuando valga 11 <br>"  ;
+    
+    $variable++;  //aqui modificamos la variable, en cada vuelta, vale uno mas. 
+}
+    
+
+?>
