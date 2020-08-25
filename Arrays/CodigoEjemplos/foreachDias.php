2cb12ac8b706d2495b8d02e178724c3a6c9ee5f6
Gsancho <luqh555@gmail.com>
2020-08-25 14:21:55 +0200

    Ejemplos

---
+++ Arrays/CodigoEjemplos/foreachDias.php
@@ -0,0 +1,23 @@
+<?php
+//gsprogramacion.com
+
+
+$dias[0]="Lunes";
+$dias[1]="Martes";
+$dias[2]="Miercoles";
+$dias[3]="Jueves";
+$dias[4]="Viernes";
+$dias[5]="Sabado";
+$dias[6]="Domingo";
+
+
+echo "Los dias de la semana son : ";
+
+foreach ($dias as $x) {
+    
+    echo " $x" ;
+    
+    
+}
+
+?>
