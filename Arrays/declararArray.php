c9fae2ccc6128b4e66a2a1ff36c0989cbe6b86cc
Gsancho <luqh555@gmail.com>
2020-08-25 11:49:44 +0200

    Ejemplos

--- Arrays/declararArray.php
+++ Arrays/declararArray.php
@@ -17,7 +17,7 @@ $planetas[] = "Marte";
 
 $planetas[] = "Jupiter";
 
-
+//Al no haber introducido ninguna clave , PHP los ordena de forma ascendiente. 
 
 echo $planetas[0] . " es el primero de los planetas del sistema solar , despues viene $planetas[1] y el quinto es $planetas[4]";
 
