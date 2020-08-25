775d2f74699fd3bf94f93e117d58ae73b1d96244
Gsancho <luqh555@gmail.com>
2020-08-25 12:06:38 +0200

    Ejemplos Array

---
+++ Arrays/declararArray3.php
@@ -0,0 +1,10 @@
+<?php
+//gsprogramacion.com
+
+//mezclamos strings y numeros como clave, no hay problema
+$planetas = array (1 => "Mercurio" , "Gigante" =>"Jupiter");
+
+
+echo "$planetas[1] es el chiquitito cercano y $planetas[Gigante] es el grandote";
+
+?>
