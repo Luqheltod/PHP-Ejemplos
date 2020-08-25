a7c56bcdde988e0d994068ffd218b4e97e322515
Gsancho <luqh555@gmail.com>
2020-08-25 12:17:03 +0200

    Ejemplos Array

---
+++ Arrays/vardumpArray.php
@@ -0,0 +1,45 @@
+<?php
+//gsprogramacion.com
+
+$planetas = array();
+
+
+$planetas[] = "Mercurio";
+
+$planetas[] = "Venus";
+
+$planetas[] = "Tierra";
+
+$planetas[] = "Marte";
+
+$planetas[] = "Jupiter";
+
+
+$planetotes = array(1=>"Mercurio" , 2=>"Venus", 3=>"Tierra" , 4=>"Marte" , 5=>"Jupiter");
+
+$planetoides = array (1 => "Mercurio" , "Gigante" =>"Jupiter");
+
+$planetones[0] = "Mercurio";
+$planetones[1] = "Venus" ;
+
+
+var_dump($planetas);
+
+echo "<br>";
+
+var_dump($planetotes);
+
+echo "<br>";
+
+var_dump($planetoides);
+
+echo "<br>";
+
+var_dump($planetones);
+
+echo "<br>";
+
+
+
+
+?>
