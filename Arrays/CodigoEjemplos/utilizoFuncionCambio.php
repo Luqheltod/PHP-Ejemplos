475ded678e075c9c04f3d77c26bd64b6e34b7732
Gsancho <luqh555@gmail.com>
2020-08-25 12:27:43 +0200

    Ejemplo

---
+++ Arrays/CodigoEjemplos/utilizoFuncionCambio.php
@@ -0,0 +1,21 @@
+<?php
+//gsprogramacion.com
+
+
+
+$planetas[] = "mercurio";
+
+$planetas[] = "venus";
+
+$planetas[] = "tierra";
+
+$planetas[] = "marte";
+
+$planetas[] = "jupiter";
+
+
+ $planetas[0] = ucwords($planetas[0]);
+
+//vemos que mercurio ha cambiado a mayusculas 
+var_dump($planetas);
+?>
