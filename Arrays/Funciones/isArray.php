c4862b9b00158379d4b1bb1734d0bfb8e43bdf0f
Gsancho <luqh555@gmail.com>
2020-08-25 14:28:37 +0200

    Ejemplos

---
+++ Arrays/Funciones/isArray.php
@@ -0,0 +1,20 @@
+<?php
+
+//gsprogramacion
+
+
+
+$unNumero = 12 ;
+
+
+
+$unArray[] = "valor";
+$unArray[] = "otroValor";
+
+
+is_array($unNumero); //nos devolverá false, puesto que no es un array
+
+is_array($unArray); //nos devolverá true
+
+
+?>
