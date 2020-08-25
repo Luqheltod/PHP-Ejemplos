57808bbeae995f7f2ef032b10c3a96b36bc7da6e
Gsancho <luqh555@gmail.com>
2020-08-25 12:10:56 +0200

    Ejemplos Arrays

---
+++ Arrays/declararArray4.php
@@ -0,0 +1,10 @@
+<?php
+//gsprogramacion.com
+
+//Usamos la otra forma de declarar arrays
+$planetas[0] = "Mercurio";
+$planetas[1] = "Venus" ;
+
+echo "Los dos primeros planetas del sistema solar son $planetas[0] y $planetas[1]";
+
+?>
