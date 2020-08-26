30e13971b51e3bee8dae29c7938039743d59b189
Gsancho <luqh555@gmail.com>
2020-08-26 17:39:04 +0200

    Ejemplo

--- Arrays/Funciones/arrayDiff.php
+++ Arrays/Funciones/arrayDiff.php
@@ -5,8 +5,7 @@
 
 //vamos a declarar 3 arrays cada uno con nombres de planetas equipos de futbol y numeros
 $array1 = array( "pequeño" =>"Mercurio" , "grande" =>"Venus" ,"mediano" =>"Tierra" , "equipo1" => "Zaragoza" );
-$array2 = array( "pequeño" =>"Elche CF" , "gigante" =>"Venus" ,"mediano" =>"Granada CF" , "obliterante"  => "Rayo Vallecano");
-$array3 = array( 25,23,66,11, "Mercurio");
+
 
 //vamos a comparar el array1 con el resto, vemos que solamente hay 2 valores que no estan en los otros 2 arrays. Estos son los que nos muestra el var_dump
 $arrayDiferencias = array_diff($array1, $array2,$array3);
