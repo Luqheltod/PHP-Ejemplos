6144af12f81c06979af77e1cf843e20e49615acf
Gsancho <luqh555@gmail.com>
2020-08-27 18:21:17 +0200

    Forms

---
+++ Formularios/formularioSencillo.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+
+<meta charset="utf-8">
+<title>Formulario Sencillo </title>
+</head>
+<body>
+
+<h2> Formulario sencillo</h2>
+
+<form method="post"  action="">
+
+Nombre: <input type="text" name = "nombre">  
+<br> <br>
+
+Email: <input type="text" name = "email">
+<br> <br> 
+
+Comentario
+ <br><br>
+<textarea name="comentario" rows="5" cols="40"></textarea>
+<br><br>
+Web: <input type="text" name = "web">
+<br><br>
+
+Genero : 
+<input type="radio" name="genero" value= "shemale"> Shemale
+<input type="radio" name="genero" value= "gay"> Hermafrodita
+<input type="radio" name="genero" value= "bizarro"> Bizarro
+<br> <br> 
+<input type="submit" name="submit" value="Submit">
+
+</form>
+
+
+
+</body>
+
+</html>
