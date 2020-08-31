a9baf49f4b1c2ce0f6e4da7297ed4f6a48483b0d
Gsancho <luqh555@gmail.com>
2020-08-31 18:51:16 +0200

    ejemplo sesiones

---
+++ Sesiones/sesionesEjemplo.php
@@ -0,0 +1,31 @@
+<?php
+
+session_start();
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> No title yet </title>
+</head>
+<body>
+
+<?php 
+
+
+$_SESSION["arrayColores"] = array("rojo" , "amarillo");
+$_SESSION["animal"] = "gato";
+
+echo $_SESSION["arrayColores"][1];
+
+echo $_SESSION["animal"];
+
+// con session_unset(); borrariamos las variables
+// con session_destroy(); cerrariamos la sesion
+
+session_destroy();
+?>
+
+</body>
+
+</html>
