de45c077ac32a1f3a853698c2efae9470b1df573
Gsancho <luqh555@gmail.com>
2020-09-01 12:26:16 +0200

    Ejemplos

---
+++ Funciones/funcionTipos.php
@@ -0,0 +1,22 @@
+<?php
+
+//gsprogramacion.com
+
+
+function soloArray (array $array){
+    
+    //esta funcion solo aceptaria arrays
+}
+
+function soloNumeros (int $numero){
+    
+    //esta funcion solo aceptaria numeros enteros
+    
+}
+
+function soloBooleans (bool $booleano){
+    
+    //esta funcion solo aceptaria booleanos
+}
+
+?>
