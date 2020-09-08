469b2225670f6ca65692de7224b764d424780a06
Gsancho <luqh555@gmail.com>
2020-09-08 18:29:00 +0200

    bbdd ejemplos

--- BBDD/FuncionConexionEjemplo/bbddFuncionConexion.php
+++ BBDD/FuncionConexionEjemplo/bbddFuncionConexion.php
@@ -1,10 +1,11 @@
 <?php
-
+//gsprogramacion.com
+//debemos incluir nuestra clase conexion.php
 require_once "Conexion.php";
 
-
-
+//llamamos a la funcion static conectar de la clase Conexion 
 $conexion = Conexion::conectar();
+//ya tenemos en nuestra variable $conexion el objeto PDO conectado a nuestra BBDD
 
 $sql = "SELECT * FROM JUGUETES";
 
