5792b5d14321c6dee0b9b5d4bed2ac6323d2bd8a
Gsancho <luqh555@gmail.com>
2020-09-08 15:16:19 +0200

    bbdd ejemplos

---
+++ BBDD/bbddContenidoAso.php
@@ -0,0 +1,22 @@
+<?php
+//gsprogramacion.com
+
+$conexion = new PDO("mysql:host=localhost; dbname=gsprogramacion" , "root","" );
+
+$sql = "SELECT * FROM JUGUETES";
+
+$resultado = $conexion->query($sql);
+
+
+
+//la funcion fetch devuelve la siguiente fila, hasta que no haya mas, en cuyo caso devolvera FALSE, por ello podemos usar un while
+while( $datos = $resultado->fetch(PDO::FETCH_ASSOC)) {
+    
+    echo "ID = " . $datos["ID"] . " NOMBRE = " . $datos["NOMBRE"] . " EDAD = " . $datos["EDAD"]
+    . " PRECIO = " . $datos["PRECIO"] . " PESO = " . $datos["PESO(GR)"];
+    echo "<br>";
+}
+
+
+
+?>
