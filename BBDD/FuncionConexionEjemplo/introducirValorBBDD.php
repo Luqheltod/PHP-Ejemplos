c80c5a508e5de73314ad804c095a987cc9450c04
Gsancho <luqh555@gmail.com>
2020-09-15 20:07:54 +0200

    bbdd

--- BBDD/FuncionConexionEjemplo/introducirValorBBDD.php
+++ BBDD/FuncionConexionEjemplo/introducirValorBBDD.php
@@ -7,7 +7,7 @@ $conexion = Conexion::conectar();
 
 
 // este tipo de comillas se consiguen con alt+96 ``
-$sql = "INSERT INTO `juguetes` (`ID`, `NOMBRE`, `EDAD`, `PRECIO`, `PESO(GR)`) VALUES (NULL, 'cualquiera', '3', '3', '3');";
+$sql = "INSERT INTO `juguetes` (`ID`, `NOMBRE`, `EDAD`, `PRECIO`, `PESO`) VALUES (NULL, 'cualquiera', '3', '3', '3');";
 
 //exec nos sirve para ejecutar INSERT, devuelve las filas afectadas.
 $filasAfectadas = $conexion->exec($sql);
