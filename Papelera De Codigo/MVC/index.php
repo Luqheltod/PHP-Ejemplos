2e3e946724d2c5a9e085186a0ecf5c778d049578
Gsancho <luqh555@gmail.com>
2020-09-21 19:40:02 +0200

    papelera

---
+++ Papelera De Codigo/MVC/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> No title yet </title>
+</head>
+<body>
+
+<h1> MVC </h1>
+
+
+<?php 
+
+require_once("controller/Animales_controlador.php");
+
+
+?>
+</body>
+
+</html>
