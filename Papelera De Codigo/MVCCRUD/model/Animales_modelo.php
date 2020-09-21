585bbf9fb53d0b1a19172fbb349cd699a4ae9c6d
Gsancho <luqh555@gmail.com>
2020-09-21 19:42:23 +0200

    papelera

---
+++ Papelera De Codigo/MVCCRUD/model/Animales_modelo.php
@@ -0,0 +1,40 @@
+<?php
+
+class Animales_model
+{
+
+    private $db;
+
+    private $animales;
+
+    public function __construct()
+    {
+
+        // se encarga de conectar con la bd , cuando creemos una instancia de animales se creara una conexion
+        require_once 'model/Conexion.php';
+
+        $this->db = Conexion::conectar();
+
+        $this->animales = array();
+    }
+
+    public function get_animales()
+    {
+        require_once("paginacion.php");  
+        
+        // en este caso vamos a obtener todos los animales.
+        $sql = "SELECT * FROM ANIMAL LIMIT $empezar_desde , $registros_paginas";
+
+        // en resultado tenemos almacenados ya los registros de los animales 
+        $resultado = $this->db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        
+        //asi que asignamos a la variable animales de la clase, el array resultado. y lo devolvemos.
+        $this->animales = $resultado;
+        
+        return $this->animales;
+    }
+}
+
+
+
+?>
