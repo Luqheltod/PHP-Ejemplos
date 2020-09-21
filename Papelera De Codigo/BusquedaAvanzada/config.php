0e9f9554ce44af2a02daea7b1529e13c25be4ab8
Gsancho <luqh555@gmail.com>
2020-09-21 19:36:09 +0200

    papelera

---
+++ Papelera De Codigo/BusquedaAvanzada/config.php
@@ -0,0 +1,14 @@
+<?php
+
+//aqui definimos sin mas las constantes de nuestra conexion , de esta forma solo tendriamos que venir aqui si se moviese.
+
+
+define("DB_HOST", "localhost");
+define("DB_USUARIO" , "root");
+define ("DB_PASSWORD" , "");
+define ("DB_NOMBRE" , "php_pildoras");
+define ("DB_CHARSET" , "utf8");
+
+
+
+?>
