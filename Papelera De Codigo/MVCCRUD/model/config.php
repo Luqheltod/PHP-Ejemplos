66c822dbcf87dd4c3af874666e374746a73d5b27
Gsancho <luqh555@gmail.com>
2020-09-21 19:42:38 +0200

    papelera

---
+++ Papelera De Codigo/MVCCRUD/model/config.php
@@ -0,0 +1,14 @@
+<?php
+
+//aca definimos sin mas las constantes de nuestra conexion ,
+
+
+define("DB_HOST", "localhost");
+define("DB_USUARIO" , "root");
+define ("DB_PASSWORD" , "");
+define ("DB_NOMBRE" , "protectora_animales");
+define ("DB_CHARSET" , "utf8");
+
+
+
+?>
