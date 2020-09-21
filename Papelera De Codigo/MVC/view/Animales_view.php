077a52896a4b2f68fe1d3a4ca13998e9b2763f61
Gsancho <luqh555@gmail.com>
2020-09-21 19:39:33 +0200

    Animales_view.php

---
+++ Papelera De Codigo/MVC/view/Animales_view.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> No title yet </title>
+</head>
+<body>
+
+<?php 
+
+
+foreach ($arrayAnimales as $value) {
+    
+    echo $value["nombre"];
+}
+
+?>
+
+</body>
+
+</html>
