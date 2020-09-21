c4b6a31aca5015d1200aea45c1e71e11d604d130
Gsancho <luqh555@gmail.com>
2020-09-21 19:43:54 +0200

    papelera

---
+++ Papelera De Codigo/MVCCRUD/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title> No title yet </title>
+<link rel="stylesheet" type="text/css" href="hoja.css">
+</head>
+<body>
+
+<?php 
+
+require_once("controller/Animales_controlador.php");
+
+
+?>
+
+</body>
+
+</html>
